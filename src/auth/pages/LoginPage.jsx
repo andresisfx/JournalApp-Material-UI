6dc6f10418fda3e9591a45eb36277bfc48e36253
andresisfx <andresrodriguezwg@gmail.com>
2025-02-15 14:16:12 -0500

    working with Button

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,4 +1,5 @@
-import {  Grid2, TextField, Typography } from '@mui/material'
+import { Google } from '@mui/icons-material'
+import {  Grid2,Button, TextField, Typography } from '@mui/material'
 
 export const LoginPage = () => {
   return (
@@ -12,30 +13,50 @@ export const LoginPage = () => {
      >
         <Grid2 item
           className='box-shadow'
-          xs={3}
-          sx={{ backgroundColor: 'white', padding: 2, borderRadius: 2 }} >
+         size={{xs: 12, sm: 6, md: 4}}
+          sx={{ backgroundColor: 'white', padding: 3, borderRadius: 2 }} >
 
           <Typography variant='h5' sx={{ mb: 1 }}>Login</Typography>
 
           <form action="">
             <Grid2  container>
-              <Grid2 item size={12} sx={{ mb: 2 }}>
-                <TextField
-                  type="email"
-                  label="Correo"
-                  placeholder='correo'
-                  fullWidth
+                <Grid2 item size={{xs: 12, md: 12}} sx={{ mb: 2 }}>
+                  <TextField
+                    type="email"
+                    label="Correo"
+                    placeholder='correo'
+                    fullWidth
+                    
+                  />
                   
-                />
+                </Grid2>
+
+                <Grid2 item size={12} sx={{ mb: 2 }}>
+                  <TextField
+                    type="password"
+                    label="contraseña"
+                    placeholder='contraseña'
+                    fullWidth
+                  />
               </Grid2>
 
-              <Grid2 item size={12} sx={{ mb: 2 }}>
-                <TextField
-                  type="password"
-                  label="contraseña"
-                  placeholder='contraseña'
-                  fullWidth
-                />
+              <Grid2 container spacing={2} sx={{mb: 2, mt: 2,}} >
+                  <Grid2 item size={{xs: 12, sm:6, md: 6}}  sx={{ mb: 1,mt: 1}}>
+
+                    <Button variant='contained' fullWidth>
+                      Login
+                    </Button>
+
+                  </Grid2>
+
+                  <Grid2 item size={{xs: 12,sm: 6, md: 6}}  sx={{ mb: 1,mt: 1 }}>
+                    
+                    <Button variant='contained' fullWidth>
+                      <Google/>
+                      <Typography>Google</Typography>
+                    </Button>
+
+                  </Grid2>
               </Grid2>
 
             </Grid2>
