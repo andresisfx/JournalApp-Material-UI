e1416596fc4dce83baaa812dfa9aac1f1746ab1c
andresisfx <andresrodriguezwg@gmail.com>
2025-02-18 23:20:48 -0500

    redux-sliced  configured

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import './styles.css'
 
 import { JournalApp } from './JournalApp.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { store } from './store'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Provider store={store}>
     <BrowserRouter>
     <JournalApp/>
     </BrowserRouter>
+    </Provider>
   </StrictMode>,
 )
