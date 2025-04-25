d0cf18bae18512583a5fc0e375be49ac6d5ae99c
andresisfx <andresrodriguezwg@gmail.com>
2025-04-24 22:26:45 -0500

    auth slice first test

--- tests/store/auth/authSlice.test.js
+++ tests/store/auth/authSlice.test.js
@@ -1,17 +1,14 @@
 import { authSlice } from "../../../src/store/auth/authSlice";
-
+import { initialState } from "../../../tests/fixtures/authFixtures.js";
 
 describe('Pruebas en authSlice', () => {
 
-    test('debe de retornar el estado por defecto', () => {
-        expect( authSlice.getInitialState() ).toEqual({
-            status: 'checking', 
-            uid: null,
-            email: null,
-            displayName: null,
-            photoURL: null,
-            errorMessage: null  
-        });
+    test('debe de retornar el estado inicial y llamarse auth', () => {
+        expect( authSlice.name).toBe('auth');
+        const state = authSlice.reducer(initialState, {});
+        console.log("aqui state",state)
+        expect(state).toEqual(initialState);
+        
     });
 });
 
